d0eeaba4cc111b7a9cd13e2b6162d3631437707f
Nafis Hasan <118046574+thenameisnafis@users.noreply.github.com>
2024-01-19 22:56:29 +0600

    Add files via upload

---
+++ sound-board/script.js
@@ -0,0 +1,25 @@
+const sounds = ['doctor-strange', 'flash-and-quicksilver', 'gasp', 'tada', 'victory', 'wrong', 'clap', 'iron-man-repulsor']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
